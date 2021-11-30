f32d2dc0d94f664852d2b1da76e7f5054035ff1f
AmirShammas <amirshammas@yahoo.com>
2021-11-30 14:54:19 +0330

    create form to add new comment

--- blog/blog1/views.py
+++ blog/blog1/views.py
@@ -1,11 +1,16 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Post
 
 class BlogListView(ListView):
-    model=Post
-    template_name='home.html'
+    model= Post
+    template_name= 'home.html'
 
 class BlogDetailView(DetailView):
-    model=Post
-    template_name='post_detail.html'
+    model= Post
+    template_name= 'post_detail.html'
     
+class BlogCreateView(CreateView):
+    model= Post
+    template_name= 'post_new.html'
+    fields= ['title', 'author', 'body']
